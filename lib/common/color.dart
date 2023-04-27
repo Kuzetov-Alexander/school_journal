fccbd40e0e31499d880fbd6885f68ae4a9d16c2b
Aleksandr19May <dela737@mail.ru>
2023-04-27 16:19:47 +0400

    in process

--- lib/common/color.dart
+++ lib/common/color.dart
@@ -3,5 +3,7 @@ import 'package:flutter/material.dart';
 class AppColors {
   static const Color greyLight = Color.fromRGBO(243, 243, 243, 1);
   static const Color greyLightSecond = Color.fromRGBO(249, 249, 249, 0.94);
-  static const Color blackAlmost = Color.fromRGBO(33, 37, 37, 1);
+  static const Color black212525 = Color.fromRGBO(33, 37, 37, 1);
+  static const Color black343434 = Color.fromRGBO(52, 52, 52, 1);
+  static const Color purple = Color.fromRGBO(86, 19, 142, 1);
 }
--- lib/features/teacher_groups/Presentation/pages/teacher_group.dart
+++ lib/features/teacher_groups/Presentation/pages/teacher_group.dart
@@ -17,12 +17,17 @@ class TeacherGroupPage extends StatelessWidget {
         leading: InkWell(
           onTap: () => context.go('/Groups'),
           child: const Image(
-          
             image: AssetImage(
                 'assets/images/arrow_left.png'), // вставляется с фоном , надо что-то придумать
           ),
         ),
-        title: Text('Группа 112',style: TextStyle(fontWeight: FontWeight.w600,fontSize: heightScreen*0.022,color: Colors.black),),
+        title: Text(
+          'Группа 112',
+          style: TextStyle(
+              fontWeight: FontWeight.w600,
+              fontSize: heightScreen * 0.022,
+              color: AppColors.black212525),
+        ),
         automaticallyImplyLeading: false,
       ),
       body: SafeArea(
@@ -31,26 +36,152 @@ class TeacherGroupPage extends StatelessWidget {
         padding: const EdgeInsets.symmetric(horizontal: 24),
         child: Column(
           children: [
+            SizedBox(height: heightScreen * 0.015),
             Row(
-              children: [Text('Ученики',style: TextStyle(fontWeight: FontWeight.w700,fontSize: heightScreen*0.024,color: AppColors.blackAlmost),), ],
+              children: [
+                Text(
+                  'Ученики',
+                  style: TextStyle(
+                      fontWeight: FontWeight.w700,
+                      fontSize: heightScreen * 0.024,
+                      color: AppColors.black212525),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: heightScreen * 0.01,
             ),
             Container(
-              height: heightScreen * 0.05,
+              height: heightScreen * 0.08,
               width: double.infinity,
               decoration: BoxDecoration(
-                  color: Colors.blue, borderRadius: BorderRadius.circular(24)),
-              child: Row(
-                children: [
-                  Text('27 учеников'),
-                  ElevatedButton(
-                      onPressed: () {},
-                      style: ButtonStyle(
-                          backgroundColor:
-                              MaterialStatePropertyAll(Colors.white)),
-                      child: Text('Все'))
-                ],
+                  color: AppColors.purple,
+                  borderRadius: BorderRadius.circular(24)),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const Text(
+                      '27 учеников',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    ElevatedButton(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.white,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(24),
+                          ),
+                        ),
+                        child: Text(
+                          'Все',
+                          style: TextStyle(
+                              color: AppColors.black212525,
+                              fontSize: heightScreen * 0.019,
+                              fontWeight: FontWeight.w600),
+                        ))
+                  ],
+                ),
               ),
-            )
+            ),
+            SizedBox(height: heightScreen * 0.015),
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                'Расписание',
+                style: TextStyle(
+                    fontWeight: FontWeight.w700,
+                    fontSize: heightScreen * 0.024,
+                    color: AppColors.black212525),
+              ),
+            ),
+            SizedBox(height: heightScreen * 0.01),
+            Container(
+              height: heightScreen * 0.3,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                  color: AppColors.purple,
+                  borderRadius: BorderRadius.circular(33)),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        IconButton(
+                            splashRadius: 1,
+                            onPressed: () {},
+                            icon: const Image(
+                                image: AssetImage(
+                                    'assets/images/arrow_left_white.png'))),
+                        Text(
+                          'Апрель',
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: heightScreen * 0.024,
+                              fontWeight: FontWeight.w600),
+                        ),
+                        IconButton(
+                            splashRadius: 1,
+                            onPressed: () {},
+                            icon: const Image(
+                                image: AssetImage(
+                                    'assets/images/arrow_right_white.png')))
+                      ],
+                    ),
+                    SizedBox(height: heightScreen*0.03,),
+                    Expanded(
+                      child: ListView.builder(
+                          shrinkWrap: true,
+                          scrollDirection: Axis.horizontal,
+                          itemCount: 15,
+                          itemBuilder: (BuildContext context, index) {
+                            return Container(
+                              decoration: BoxDecoration(border: Border.all(width: 2,color: AppColors.purple.withOpacity(0.4),)),
+                              height: heightScreen * 0.02,
+                             
+                              width: widthScreen * 0.1,
+                            
+                              margin: EdgeInsets.symmetric(
+                                  horizontal: widthScreen * 0.03),
+                            );
+                          }),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left:16.0,right: 16, top: 16,bottom: 32),
+                      child: SizedBox(
+                        height: 56,
+                        width: double.infinity,
+                        child: TextButton(
+                          style: ButtonStyle(
+                            backgroundColor:
+                                const MaterialStatePropertyAll<Color>(
+                                    Colors.white),
+                            tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                            shape: MaterialStateProperty.all(
+                              RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(18),
+                              ),
+                            ),
+                          ),
+                          onPressed: () {},
+                          child: const Text(
+                            'Изменить расписание',
+                            style: TextStyle(
+                              color: AppColors.black343434,
+                              fontSize: 18,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
           ],
         ),
       ))),
